15e15448a6e08ac967f0e0ca3c11fb966cc57de7
Mark W. Burgoyne <mark.w.burgoyne@gmail.com>
2022-01-07 11:59:33 +1030

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from distutils.core import setup
 
-setup(
-    name = 'pyrestoolbox',
-    packages = ['pyrestoolbox'],
-    version = 'v1.0.0',  # Ideally should be same as your GitHub release tag varsion
-    description = 'pyResToolbox - A collection of Reservoir Engineering Utilities',
-    author = 'Mark W. Burgoyne',
-    author_email = 'mark.w.burgoyne@gmail.com',
-    url = 'https://github.com/mwburgoyne/pyResToolbox',
-    download_url = 'https://github.com/mwburgoyne/pyResToolbox/archive/refs/tags/v1.0.0.tar.gz',
-    keywords = ['restoolbox', 'petroleum', 'reservoir'],
-    classifiers = [],
-    REQUIRED = ['numpy', 'scipy', 'pandas', 'tabulate', 'enum', 'gwr_inversion'],
-)
+
+name = 'pyrestoolbox'
+packages = ['pyrestoolbox']
+version = 'v1.0.0',  # Ideally should be same as your GitHub release tag varsion
+description = 'pyResToolbox - A collection of Reservoir Engineering Utilities'
+author = 'Mark W. Burgoyne'
+author_email = 'mark.w.burgoyne@gmail.com'
+url = 'https://github.com/mwburgoyne/pyResToolbox'
+download_url = 'https://github.com/mwburgoyne/pyResToolbox/archive/refs/tags/v1.0.0.tar.gz'
+keywords = ['restoolbox', 'petroleum', 'reservoir']
+classifiers = []
+REQUIRED = ['numpy', 'scipy', 'pandas', 'tabulate', 'enum', 'gwr_inversion']
