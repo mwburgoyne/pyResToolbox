0b4e5539646d3d5be7ffe806435c8243066d8c2b
Mark W. Burgoyne <mark.w.burgoyne@gmail.com>
2022-01-30 19:21:21 +1030

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,23 +3,23 @@ from setuptools import setup, find_packages
 import importlib
 import collections
 import os
+import pkg_resources
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 f = open('C:\\Users\\vinom\OneDrive - Santos\\Work in progress\\ML Learning tidbits\\0-ResEng\\pyResToolbox\\source_dir\\README.md', 'r').read()
 long_description= f #markdown.markdown( f.read() )
-#long_description = open("README.md").read_text()
 
 setup(
     name = 'pyrestoolbox',
     packages = find_packages(),
-    version = '1.1.4',  # Ideally should be same as your GitHub release tag varsion
+    include_package_data=True,
+    version = '1.2.0',  # Ideally should be same as your GitHub release tag varsion
     description = 'pyResToolbox - A collection of Reservoir Engineering Utilities',
     long_description= long_description,
     long_description_content_type = 'text/markdown',
     author = 'Mark W. Burgoyne',
     author_email = 'mark.w.burgoyne@gmail.com',
     url = 'https://github.com/mwburgoyne/pyResToolbox',
-    #download_url = 'https://github.com/mwburgoyne/pyResToolbox/archive/refs/tags/v1.0.2.tar.gz',
     keywords = ['restoolbox', 'petroleum', 'reservoir'],
     classifiers = [],
     install_requires=[
